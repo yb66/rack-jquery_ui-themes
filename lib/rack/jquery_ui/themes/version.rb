a49ca581acf721f05fbbeac63fcb63f5206181a0
Iain Barnett <iainspeed@gmail.com>
2013-07-18 08:20:52 +0100

    Bumped the jQuery UI version number.

--- lib/rack/jquery_ui/themes/version.rb
+++ lib/rack/jquery_ui/themes/version.rb
@@ -2,7 +2,7 @@ module Rack
   class JQueryUI
 
     # This is here in case another rack-jquery_ui library has already been loaded.
-    jquery_ui_version = "1.10.1"
+    jquery_ui_version = "1.10.3"
     if defined? JQUERY_UI_VERSION
       warn "JQUERY_UI_VERSION was already defined."
       unless JQUERY_UI_VERSION == jquery_ui_version
@@ -14,7 +14,7 @@ module Rack
 
       # This is the release date of the jQuery file, it makes an easy "Last-Modified" date for setting the headers around caching.
       # @todo remember to change Last-Modified with each release!
-      JQUERY_UI_VERSION_DATE = "Fri, 15 Feb 2013 00:00:00 GMT"
+      JQUERY_UI_VERSION_DATE = "Fri, 03 May 2013 00:00:00 GMT"
     end
 
     class Themes
